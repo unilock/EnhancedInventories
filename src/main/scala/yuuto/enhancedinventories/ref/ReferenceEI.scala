7fb5fedfa88354e6a549055462c848185b35c1c2
unilock <unilock@fennet.rentals>
2024-01-22 09:57:00 -0500

    Use Tags.VERSION
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/scala/yuuto/enhancedinventories/ref/ReferenceEI.scala
+++ src/main/scala/yuuto/enhancedinventories/ref/ReferenceEI.scala
@@ -3,15 +3,17 @@
  */
 package yuuto.enhancedinventories.ref
 
+import yuuto.enhancedinventories.Tags
+
 final object ReferenceEI {
   final val MOD_ID= "EnhancedInventories";
   final val MOD_NAME = "Enhanced Inventories";
-  final val VERSION = "1.7.10-1.1.11";
+  final val VERSION = Tags.VERSION;
   final val MOD_PACKAGE = "yuuto.enhancedinventories";
-  
+
   final val PROXY_CLIENT = MOD_PACKAGE+".proxy.ProxyClient";
   final val PROXY_SERVER = MOD_PACKAGE+".proxy.ProxyServer";
   final val CHANNEL = "EnhancedInventories";
-  
+
 
 }
